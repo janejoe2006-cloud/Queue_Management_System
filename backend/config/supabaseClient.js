4cc3719233d9d34ef38dc224c3be37ab14764696
Jane Joe <janejoe2006@gmail.com>
2026-09-19 10:23:28 +0530

    Updated config

--- backend/config/supabaseClient.js
+++ backend/config/supabaseClient.js
@@ -1,8 +1,22 @@
-const { createClient } = require("@supabase/supabase-js");
+/*const { createClient } = require("@supabase/supabase-js");
 
 const supabase = createClient(
     process.env.SUPABASE_URL,
     process.env.SUPABASE_KEY
+);
+
+module.exports = supabase;*/
+const { createClient } = require("@supabase/supabase-js");
+const ws = require("ws");
+
+const supabase = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_KEY,
+    {
+        realtime: {
+            transport: ws
+        }
+    }
 );
 
 module.exports = supabase;
